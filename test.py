9feb7d4f7312d064b3b1b8c2d1042ee4964ff012
Clement Schaff <clement.schaff@gmail.com>
2017-07-25 22:10:29 +0200

    delete part moved to trash

--- test.py
+++ test.py
@@ -43,17 +43,3 @@ base = ie_preprocess_col(base)
 export_decision(appel_prudhomme, 2)
 
 
-part_separator = ['ENTRE :', 'ET :', "L'affaire a été appelée à l'audience publique", 'MOTIFS DE LA DÉCISION :', 'PAR CES MOTIFS', ]
-
-# cut the decision in its main parts.
-import re
-
-def split_text_arret(text):
-    remain = text
-    [HEAD, remain] = re.split('ENTRE :', remain, maxsplit = 1)
-    [PARTIE1, remain] = re.split('ET :\n', remain, maxsplit = 1)
-    [PARTIE2, remain] = re.split("L'affaire a été appelée à l'audience publique", remain, maxsplit = 1)
-    [ESPECE, remain] = re.split('MOTIFS DE LA DÉCISION :\n', remain, maxsplit = 1)
-    [MOTIFS, DECISION] = re.split('PAR CES MOTIFS\n', remain, maxsplit = 1)
-
-    
